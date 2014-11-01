fcb8aa1d6153de8eb0e967ec6035ad246e84b353
slopjong <git@slopjong.de>
2014-11-01 19:47:54 +0100

    Replaced IDs with classes

--- web/jsplumb/demo.js
+++ web/jsplumb/demo.js
@@ -12,80 +12,64 @@ jsPlumb.ready(function() {
     // don't necessarily need to specify a lineWidth
     var hoverPaintStyle = { strokeStyle:"#7ec3d9" };
 
-    var w23Stroke = "rgb(189,11,11)"; 
+    var w23Stroke = "rgb(189,11,11)";
+
     var connection3 = instance.connect({
-		source:"window2", 
-		target:"window3", 
+		source: "window2",
+		target: "window3",
 		paintStyle:{ 
-			lineWidth:8,
-			strokeStyle:w23Stroke,
-			outlineColor:"#666",
-			outlineWidth:1 
+			lineWidth:1,
+			strokeStyle: w23Stroke,
+			outlineColor: "#666",
+			outlineWidth: 1
 		},
-		detachable:false,
-		hoverPaintStyle:hoverPaintStyle, 
-		anchors:[ [ 0.3 , 1, 0, 1 ], "TopCenter" ], 
-		endpoint:"Rectangle", 
+		detachable: false,
+		hoverPaintStyle: hoverPaintStyle,
+		anchors:[ [ 0.3 , 1, 0, 1 ], "Top" ],
+		endpoint:"Rectangle",
 		endpointStyles:[
 			{ gradient : { stops:[[0, w23Stroke], [1, "#558822"]] }},
 			{ gradient : {stops:[[0, w23Stroke], [1, "#882255"]] }}
 		]	
-	});					
-		
-	var connection2 = instance.connect({
-		source:'window3', target:'window4', 
-		paintStyle:{ 
-		   lineWidth:10,
-		   strokeStyle:connectorStrokeColor,
-		   outlineColor:"#666",
-		   outlineWidth:1
-		},
-		hoverPaintStyle:hoverPaintStyle, 
-		anchor:"AutoDefault",
-		detachable:false,
-		endpointStyle:{ 
-			   gradient : { 
-				   stops:[[0, connectorStrokeColor], [1, connectorHighlightStrokeColor]],
-				   offset:17.5, 
-				   innerRadius:15 
-			   }, 
-			   radius:35
-		},				        					        			
-		label : function(connection) { 
-			var d = new Date();
-			var fmt = function(n, m) { m = m || 10;  return (n < m ? new Array(("" + m).length - (""+n).length + 1).join("0") : "") + n; }; 
-			return (fmt(d.getHours()) + ":" + fmt(d.getMinutes()) + ":" + fmt(d.getSeconds())+ "." + fmt(d.getMilliseconds(), 100)); 
-		},
-		labelStyle:{
-			cssClass:"component label"
-		}
-   });
-
-    var connection5 = instance.connect({
-		source:"window4", 
-		target:"window5", 
-		anchors:["BottomRight", "TopLeft"], 
-		paintStyle:{ 
-			lineWidth:7,
-			strokeStyle:"rgb(131,8,135)",
-//										outlineColor:"#666",
-//						 				outlineWidth:1,
-			dashstyle:"4 2",
-			joinstyle:"miter"
-		},
-		hoverPaintStyle:hoverPaintStyle, 
-		endpoint:["Image", {url:"endpointTest1.png"}], 
-		connector:"Straight", 
-		endpointsOnTop:true,
-		overlays:[ ["Label", {
-						cssClass:"component label",		    			        				 
-						label : "4 - 5",
-						location:0.3
-					}],
-					"Arrow"
-					
-		]
 	});
+
+    var connection3 = instance.connect({
+        source: "window5",
+        target: "window2",
+        paintStyle:{
+            lineWidth:1,
+            strokeStyle: w23Stroke,
+            outlineColor: "#666",
+            outlineWidth: 1
+        },
+        detachable: false,
+        hoverPaintStyle: hoverPaintStyle,
+        anchors:[ [ 0.3 , 1, 0, 1 ], "Top" ],
+        endpoint:"Rectangle",
+        endpointStyles:[
+            { gradient : { stops:[[0, w23Stroke], [1, "#558822"]] }},
+            { gradient : {stops:[[0, w23Stroke], [1, "#882255"]] }}
+        ]
+    });
+
+    var connection3 = instance.connect({
+        source: "window4",
+        target: "window5",
+        paintStyle:{
+            lineWidth:1,
+            strokeStyle: w23Stroke,
+            outlineColor: "#666",
+            outlineWidth: 1
+        },
+        detachable: false,
+        hoverPaintStyle: hoverPaintStyle,
+        anchors:[ [ 0.3 , 1, 0, 1 ], "Top" ],
+        endpoint:"Rectangle",
+        endpointStyles:[
+            { gradient : { stops:[[0, w23Stroke], [1, "#558822"]] }},
+            { gradient : {stops:[[0, w23Stroke], [1, "#882255"]] }}
+        ]
+    });
 
 	// jsplumb event handlers
 
@@ -106,7 +90,7 @@ jsPlumb.ready(function() {
 	//instance.draggable(jsPlumb.getSelector(".window"), { containment:".demo"});    
 	instance.draggable(jsPlumb.getSelector(".window"), {
 		drag:function() {
-			//console.log("DRAG")
+			// console.log("DRAG")
 		}
 	});    
 
